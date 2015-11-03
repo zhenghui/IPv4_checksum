d497a75704b28c576b7766a72aefb94ea741ddea
zhenghui.cjb <zhenghui.cjb@taobao.com>
2015-11-03 19:10:16 +0800

    refactor

--- src/main/scala/zhenghui/IPv4Util.scala
+++ src/main/scala/zhenghui/IPv4Util.scala
@@ -17,8 +17,8 @@ object IPv4Util {
       case Array(Numner10(p1),Numner10(p2),Numner10(p3),Numner10(p4),Numner10(p5),Numner10(p6),Numner10(p7),Numner10(p8),Numner10(p9),Numner10(p10)) =>
         p1+p2+p3+p4+p5+p7+p8+p9+p10 match {
           case BinaryTuple(head,tail) =>
-            val checkCode = BigInt(Integer.valueOf(fill0((head + tail).toString(BINARY_NUM),16).map(c => if(c == '1')'0'else '1').toString,BINARY_NUM))
-            List(p1,p2,p3,p4,p5,checkCode,p7,p8,p9,p10).map(s=>fill0(s.toString(HEX_NUM),4)).mkString(" ")
+            val checkCode = BigInt(Integer.valueOf((head + tail).toString(BINARY_NUM).reverse.padTo(16,'0').reverse.map(c => if(c == '1')'0'else '1').toString,BINARY_NUM))
+            List(p1,p2,p3,p4,p5,checkCode,p7,p8,p9,p10).map(s=>s.toString(HEX_NUM).reverse.padTo(4,'0').reverse).mkString(" ")
           case _ => "" //ignore
         }
       case _ =>
@@ -26,22 +26,8 @@ object IPv4Util {
     }
   }
 
-  def fill0(str:String,num:Int):String = {
-    if(str.length >= num) str else {
-      var remainders = num - str.length
-      var reverseStr = str.reverse
-      while(remainders > 0){
-        reverseStr += "0"
-        remainders -= 1
-      }
-      reverseStr.reverse
-    }
-  }
-
   object Numner10 {
-    def unapply(str:String): Option[BigInt] ={
-      Try{Integer.parseInt(str,HEX_NUM)}.toOption.flatMap(i => Some(BigInt(i)))
-    }
+    def unapply(str:String): Option[BigInt] = Try{Integer.parseInt(str,HEX_NUM)}.toOption.flatMap(i => Some(BigInt(i)))
   }
 
   object BinaryTuple{
@@ -54,8 +40,4 @@ object IPv4Util {
     }
   }
 
-  def main(args: Array[String]) {
-    println(checksum("4500 0042 3038 0000 4011 0000 c0a8 0afa af90 6ce9"))
-    //"4500 0042 3038 0000 4011 6257 c0a8 0afa af90 6ce9"
-  }
 }
